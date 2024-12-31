0a0e6e729d1074788410c4a7972dd09e7ae364ff
PsSergio <pscosta2008@gmail.com>
2024-12-31 12:51:46 -0300

    feat: adding cellphone response in home-page

--- chezz-website/src/components/UserInfo.jsx
+++ chezz-website/src/components/UserInfo.jsx
@@ -1,6 +1,25 @@
 import LogoutImage from '../assets/logout.png'
 
-function UserInfo({username, rating}){
+function UserInfo({username, rating, mode}){
+
+    if(mode == "Cellphone"){
+
+        return(
+            <div className="user-info-container-cellphone default-color add-shadow w-screen flex p-5 justify-start items-center mb-12">
+                
+                <img className="profile-img" src="https://picsum.photos/100" alt="Profile image" width={50} height={50}/>
+                <div className="user-info">
+                    <div>
+                        <p className="username">{username}</p>
+                        <p className="rating">({rating})</p>
+                    </div>
+
+                    <img src={LogoutImage} alt="" className="logout" width={20}/>
+                </div>
+            </div>
+        )
+
+    }
 
     return (
 
@@ -13,7 +32,7 @@ function UserInfo({username, rating}){
                     <p className="rating">({rating})</p>
                 </div>
 
-                <img src={LogoutImage} alt="" className="logout" width={20}/>
+                <img src={LogoutImage} alt="" className="logout" width={20} height={20}/>
             </div>
         </div>
 
--- chezz-website/src/pages/HomePage.jsx
+++ chezz-website/src/pages/HomePage.jsx
@@ -2,17 +2,31 @@ import Board from '../components/Board';
 import Logo from '../components/Logo';
 import PlayButton from '../components/PlayButton';
 import UserInfo from '../components/UserInfo';
-// import { boardSize } from '../globals';
 
 export default function HomePage(){
     return (
 
         <section className="container-bkg">
-            <div className='container-home'>
+            <div className="container-home-phone-response phone-mode">
+                <div className="userInfo">
+                    <UserInfo username={"PsCosta"} rating="1500" mode={"Cellphone"}/>
+                </div>
+                <div className='board-container w-screen'>
+                    <div className="board">
+                        <Board/>
+
+                    </div>
+                    <div className="buttons-container">
+                        <PlayButton text={"Jogar online"}/>
+                        <PlayButton text={"Jogar com bots"}/>
+                    </div>
+                </div>
+            </div>
+            <div className='container-home-desktop-response desktop-mode'>
                 <div className="left-side-home ">
                     <Logo _width={200}/>
 
-                    <UserInfo username={"PsCosta"} rating="1500"/>
+                    <UserInfo username={"PsCosta"} rating="1500" mode={"Desktop"}/>
 
                     <div className='buttons-container default-color add-shadow mb-4 mt-6'>
 
